52395b3c45c81d79ed7798bc7f98b6190784b2cf
Felipe Kenji <68760458+Fekenji@users.noreply.github.com>
2024-12-03 18:12:25 +0000

    Fix image

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,8 @@ function Home() {
         color: '#555',
     };
 
-    return (
-        <img href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_WY2zAvpUOYoTQCA3JNfChOHhulHNp3yzA&s"/>
-    );
+    return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_WY2zAvpUOYoTQCA3JNfChOHhulHNp3yzA&s"/>
+
 }
 
 export default Home;
