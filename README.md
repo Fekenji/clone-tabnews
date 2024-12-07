5fabc29a2d26e44176ae5621b914cd1228e8552c
Felipe Kenji <68760458+Fekenji@users.noreply.github.com>
2024-12-07 00:57:18 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprendizado seguindo o curso.dev, cuja função é acumular conteúdos sobre tecnologia
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_WY2zAvpUOYoTQCA3JNfChOHhulHNp3yzA&s"/>
+  return (
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_WY2zAvpUOYoTQCA3JNfChOHhulHNp3yzA&s" />
+  );
 }
 
 export default Home;
