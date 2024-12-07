92b99ce6600c1c61caed2aa93b85a66c99dd789b
Felipe Kenji <68760458+Fekenji@users.noreply.github.com>
2024-12-07 00:30:59 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,5 @@
 function Home() {
-    const containerStyle = {
-        maxWidth: '600px',
-        margin: '20px auto',
-        padding: '10px',
-        borderRadius: '8px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        backgroundColor: '#f5f5f5',
-    };
-    const titleStyle = {
-        fontWeight: 'bold',
-        fontSize: '24px',
-        fontFamily: 'Arial, sans-serif',
-        marginTop: '20px',
-        color: '#333',
-    };
-    const itemStyle = {
-        fontSize: '18px',
-        fontFamily: 'Arial, sans-serif',
-        marginLeft: '20px',
-        color: '#555',
-    };
-
     return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_WY2zAvpUOYoTQCA3JNfChOHhulHNp3yzA&s"/>
-
 }
 
 export default Home;
